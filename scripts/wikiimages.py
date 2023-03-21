c54270d7dea11e09fb4695f434e7d254b1dd69f3
Léo Filoche <leofiloche.wk@gmail.com>
2023-03-21 09:47:36 +0100

    Reorga fichiers vers dossier script

--- scripts/wikiimages.py
+++ scripts/wikiimages.py
@@ -25,32 +25,8 @@ def get_wiki_image_link(search_term,lang='fr'):
 
 
 import requests 
-import urllib.request
-import mimetypes
 from PIL import Image
-
-def getExtension(filename):
-    contentType, _ = mimetypes.guess_type(filename)
-    if(contentType=="image/svg+xml"):return ".svg"
-    elif(contentType=="image/png"):return ".png"
-    elif(contentType=="image/jpg"):return ".jpg"
-    elif(contentType=="image/jpeg"):return ".jpeg"
-    elif(contentType=="image/gif"):return ".gif"
-    else : return ""
-
-def loadImage(url,file_name,save=True):
-    extension = getExtension(url)
-    file_name+=extension
-    res = requests.get(url, stream = True)
-    if res.status_code == 200 or res.status_code == 403:
-        opener=urllib.request.build_opener()
-        opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
-        urllib.request.install_opener(opener)
-        if(save):name,http =  urllib.request.urlretrieve(url,file_name)
-        else: name,http =  urllib.request.urlretrieve(url)
-        img = Image.open(name)
-        return True,file_name,img
-    return False,None,None
+import imageloader
 
 
 def loadWikiImage(search_term,destdir='images/',nospace=False,save=False,lang='en'):
@@ -59,7 +35,7 @@ def loadWikiImage(search_term,destdir='images/',nospace=False,save=False,lang='e
         return False,None,None
     if nospace: #On enlève les underscore (cela peut être à l'origine d'erreurs)
         name = name.replace(" ","_")
-    return loadImage(wiki_image_url,destdir+name,save)
+    return imageloader.loadImage(wiki_image_url,destdir+name,save)
     
         
 # sucss,_,img = loadWikiImage("Thomas Pesquet",destdir='web/images/',nospace=True)
