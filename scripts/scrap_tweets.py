de0160088713bd94f22e9ded7efe5eef7e5bc807
Kali-ki <kilian.cornec@protonmail.com>
2023-04-21 15:16:24 +0200

    Perfect csv created to make search on dataset

---
+++ scripts/scrap_tweets.py
@@ -0,0 +1,18 @@
+import stweet as st
+import arrow
+
+def try_search(username, date_start, date_end, filter=st.RepliesFilter.ONLY_ORIGINAL):
+    search_tweets_task = st.SearchTweetsTask(from_username=username, since=date_start, until=date_end, replies_filter=filter)
+    output_jl_tweets = st.JsonLineFileRawOutput('output_raw_search_tweets.jl')
+    output_jl_users = st.JsonLineFileRawOutput('output_raw_search_users.jl')
+    output_print = st.PrintRawOutput()
+    st.TweetSearchRunner(search_tweets_task=search_tweets_task,
+                         tweet_raw_data_outputs=[output_print, output_jl_tweets],
+                         user_raw_data_outputs=[output_print, output_jl_users]).run()
+
+# Loop on year, month, day since 2010-01-01 to 2022-01-01
+d1 = arrow.Arrow(2010, 1, 1)
+d2 = arrow.Arrow(2023, 1, 1)
+while d1 < d2:
+    try_search("TopHashtags", date_start=d1, date_end=d1.shift(days=+1))
+    d1 = d1.shift(days=+1)
--- scripts/tweets_shaping.py
+++ scripts/tweets_shaping.py
@@ -1,14 +1,39 @@
-import numpy as np
 import pandas as pd
 
+# Function to parse elements of a tweet
+def parse(tweet, kw):
+    res = dict(tweet)[kw]
+    return res
+
+# Function to parse the hashtags
+def parse_hashtags(list_hashtags):
+    res = []
+    for i in range(len(list_hashtags)):
+        res.append(list_hashtags[i]["text"])
+    return res
+
 # Read JSON file with pandas
-tweets = pd.read_json('../data/tweet_dataset.json', lines=True)
+df_tweets_ = pd.read_json('../data/tweet_dataset.json')
 
-# Get only "rawdata" part
-tweets = tweets['raw_value']
+# Select only the raw_value column
+df_tweets_ = df_tweets_["raw_value"]
 
-# Cast in dictionary
-tweets = tweets.to_dict()
+# Create empty dataframe
+df_tweets = pd.DataFrame()
 
-# Get only the text of the tweets
-print(tweets[0])
+# Create a list of keys to extract
+keys = ["created_at", "full_text", "entities", "lang"]
+# Extract the keys and add them to the dataframe
+for key in keys :
+    df_tweets[key] = df_tweets_.apply(parse, kw = key)
+
+# Extract the hashtags
+df_tweets_entities = df_tweets["entities"]
+df_tweets_hashtags = pd.DataFrame(df_tweets_entities.apply(parse, kw = "hashtags"))["entities"]
+df_tweets["hashtags"] = df_tweets_hashtags.apply(parse_hashtags)
+
+# Drop the entities column
+df_tweets.drop(columns = ["entities"], inplace = True)
+
+# Save the dataframe as a CSV file
+df_tweets.to_csv('../data/tweets.csv', index = False)
