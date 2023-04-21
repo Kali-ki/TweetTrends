ad6da4e8b9b88c9859410abcc9e93c8ae254786e
Kali-ki <kilian.cornec@protonmail.com>
2023-04-21 18:44:40 +0200

    Improve of getting most used hashtags

--- scripts/process_tweets_data.py
+++ scripts/process_tweets_data.py
@@ -25,22 +25,33 @@ for year in range(2010, 2023):
 base = 2010
 number_of_years = 12
 
+# Loop over the list of dataframes
 for i in range(number_of_years+1):
 
+    # Get the hashtags of the tweets of the year
     df_tweets_year_i = list_df_tweets[i]
     df_tweets_year_i_hashtags = df_tweets_year_i["hashtags"]
+
+    # Reset index
     df_tweets_year_i_hashtags.reset_index(drop = True, inplace = True)
 
+    # Convert the hashtags from string to list
     list_hashtags_year_i = []
-
     for j in range(len(df_tweets_year_i_hashtags)):
-        l = ast.literal_eval(df_tweets_year_i_hashtags[j])
+        # Upper the hashtags
+        l = df_tweets_year_i_hashtags[j].upper()
+        l = ast.literal_eval(l)
         list_hashtags_year_i += l
 
-    # Count the number of occurences of each hashtag
+    # Convert the list to a pandas series
     series_hashtags_year_i = pd.Series(list_hashtags_year_i)
+    
+    # Drop the hashtags that are not wanted
+    list_not_wanted_hashtags = ["CONCOURS", "RT", "FB", "FF", "LT", "ON", "LRT", "FACEBOOK"]
+    for hashtag in list_not_wanted_hashtags:
+        series_hashtags_year_i = series_hashtags_year_i[series_hashtags_year_i != hashtag]
 
-    # Get the 10 most popular hashtags
-    print("Most used hashatag in year",str(base+i),":")
-    print(series_hashtags_year_i.value_counts().head(10),"\n")
+    # Get the 15 most popular hashtags
+    print("Most used hashatag in year", str(base+i), ":")
+    print(series_hashtags_year_i.value_counts().head(15),"\n")
     
