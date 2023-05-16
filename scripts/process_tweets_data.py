70b3e0ce46e057388292452e66586630853282aa
Kali-ki <kilian.cornec@protonmail.com>
2023-05-16 14:47:59 +0200

    Add context link for each hashtag

--- scripts/process_tweets_data.py
+++ scripts/process_tweets_data.py
@@ -1,18 +1,21 @@
 import pandas as pd
 import ast
 
+def generate_link(hashtag, year):
+    link = "https://twitter.com/search?q=(%23" + hashtag + ")%20lang%3Afr%20until%3A" + year + "-12-31%20since%3A" + year + "-01-01&src=typed_query"
+    return link
+
 # Read CSV file with pandas
 df_tweets = pd.read_csv('../data/tweets.csv')
 
 # Keep only tweets with lang='fr'
-import ast
 df_tweets = df_tweets[df_tweets["lang"] == "fr"]
-
-# Reset index
-df_tweets.reset_index(drop = True, inplace = True)
 
 # Drop the lang column
 df_tweets.drop(columns = ["lang"], inplace = True)
+
+# Reset index
+df_tweets.reset_index(drop = True, inplace = True)
 
 # List of 12 dataframes, one for each year
 list_df_tweets = []
@@ -23,10 +26,11 @@ for year in range(2010, 2023):
 
 # --- Get the most popular hashtags for year 2010 to 2022 ---
 
+# Set the base year and the number of years to consider
 base = 2010
 number_of_years = 12
 
-# Create new DataFrame
+# Create new DataFrame to store the most used hashtags for each year
 df_hashtags = pd.DataFrame()
 
 # Loop over the list of dataframes
@@ -39,11 +43,12 @@ for i in range(number_of_years+1):
     # Reset index
     df_tweets_year_i_hashtags.reset_index(drop = True, inplace = True)
 
-    # Convert the hashtags from string to list
+    # Convert the hashtags from string to list and store them in a list
     list_hashtags_year_i = []
     for j in range(len(df_tweets_year_i_hashtags)):
         # Upper the hashtags
         l = df_tweets_year_i_hashtags[j].upper()
+        # Convert the string to a list
         l = ast.literal_eval(l)
         list_hashtags_year_i += l
 
@@ -61,16 +66,21 @@ for i in range(number_of_years+1):
     # Keep only the 10 most popular hashtags
     series_hashtags_year_i = series_hashtags_year_i.value_counts().head(10)
 
-    # Get the 15 most popular hashtags
+    # Get the 10 most popular hashtags
     print("Most used hashatag in year", str(base+i), ":")
     print(series_hashtags_year_i,"\n")
 
-    series_hashtags_year_i = series_hashtags_year_i.reset_index()
-    series_hashtags_year_i.columns = [base+i, "score "+str(base+i)]
+    # Generate the link for each hashtag and store them in a series
+    series_link = pd.Series(series_hashtags_year_i.index).apply(lambda tuple: generate_link(tuple, str(base+i)))
+    series_link.name = "context link"
 
-    # Add the series to the dataframe
-    df_hashtags = pd.concat([df_hashtags, series_hashtags_year_i], axis = 1)
+    # Place hashtags as columns and their score as values -> convert the series to a dataframe
+    df_hashtags_year_i = series_hashtags_year_i.reset_index()
+    # Rename the columns
+    df_hashtags_year_i.columns = [base+i, "score "+str(base+i)]
+
+    # Merge the dataframe and the series in df_hashtags
+    df_hashtags = pd.concat([df_hashtags, df_hashtags_year_i, series_link], axis = 1)
 
 # Save the dataframe to a csv file
 df_hashtags.to_csv("../data/most_used_hashtags.csv", index = False)
- 
