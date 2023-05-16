3c11e5675263397ccb2fdc2d71e2f8959eb1a842
Léo Filoche <leofiloche.wk@gmail.com>
2023-05-16 09:21:13 +0200

    Sauvegarde des recherches précédentes d'image dans un json. TOFIX : si aucune image d'illustration n'est trouvée, il y a un doublon de l'image suivante

--- scripts/ui.py
+++ scripts/ui.py
@@ -7,17 +7,113 @@ import glob
 from wikiparser import WikiParser
 from gimageserpapiparser import GImageSerpApiParser
 import shutil
+import json
 
 
 UI_FOLDER='web'
 
 
+def startui(save_ui=True):
+   if not os.path.exists(UI_FOLDER): os.mkdir(UI_FOLDER)
+   _initHTMLCSSJSFiles()
+   _createImagesFolder()
+   eel.init(UI_FOLDER)  
+
+   @eel.expose
+   def loadPeriodPy(periodid):
+    '''
+    Applies changes on the UI, according to a new period selection by the user
+    The keywords of the periods are retreived
+    The  illustration images are downloaded and their background are removed
+    '''
+    wikip = WikiParser
+    gisp = GImageSerpApiParser
+    keywords = _updateKeywords(periodid)
+    eel.loadPeriodJs(_illustrate_keywords(keywords,[wikip, gisp]))
+
+   def close(route,sockets):
+       if not save_ui : _deleteUIfolder()
+       exit()
+   eel.start('index.html',close_callback=close,size=(700,700))
+
+
+
+####################
+#     INIT UI      #
+###################
+
 def _createImagesFolder():
     if not os.path.exists(UI_FOLDER+"/images"):  os.mkdir(UI_FOLDER+'/images')
 
-def _deleteUIfolder(_,__):
+def _deleteUIfolder():
     if os.path.exists(UI_FOLDER):shutil.rmtree(UI_FOLDER)
-    exit()
+
+
+def _updateKeywords(period):
+    #TODO: interact with tweeter (by example) to find keywords fitting to a period
+    return ["griezmann","ironsd","mario"]
+    
+def _initPeriods():
+    #TODO: load all available periods on init 
+    return ["Decembre 2020", "Janvier 2021","Fevrier 2021"]
+
+def _initHTMLCSSJSFiles():
+    indexlines = []
+    # read the template
+    with open('../uitemplate.html', 'r') as template:
+        for line in template:
+            if '<!--datestopropose-->' in line:
+                # create dropdown list containing all the available periods
+                for period in _initPeriods():
+                    name = period.replace(' ','-').lower()
+                    option = f'<option value="{name}">{period}</value>'
+                    indexlines.append(option)
+            else :  indexlines.append(line.strip())
+
+    # write the ui file
+    with open(UI_FOLDER+"/index.html", 'w') as fileindex:
+        fileindex.write('\n'.join(indexlines))
+
+
+##########################
+#  KEYWORDS ILLUSTRATION #
+##########################
+
+
+def _illustrate_keywords(keywords : list[str],parsers : list[ImageParser],save_illustrations=True):
+    '''
+    tries to illustrate the maximum of keywords with the parsers.
+    returns a list of     objects with the shape :  
+    { keyword,  url, illustration} indicating all the illustrated keywords.
+    url and illustration can be empty strings if the parsers failed to find an illustration.
+    '''
+    keywords_existing_illustrations = _getSavedKeywordsIllustrations()
+    keywords_url = {key: '' for key in keywords}
+    for keyword in keywords:
+        if keyword in keywords_existing_illustrations:
+            keywords_url[keyword] = keywords_existing_illustrations[keyword]['url']
+    for parser in parsers:
+        keywords_url = _findImageLinks(parser,keywords_url)
+    
+    #Resets the images folder content and loads all the new images in it
+    # for f in glob.glob(UI_FOLDER+"/images/*"):os.remove(f)
+    keywords_infos = []
+    for keyword in list(keywords_url.keys()):
+        url = keywords_url[keyword]
+        illustration = _imagenameformat(url, keyword)
+        if not os.path.exists(UI_FOLDER+'/images/' + illustration):
+            saveImage(url, keyword)
+        keywords_infos.append({
+            "keyword":keyword,
+            "url":url,
+            "illustration":illustration
+        })
+    if save_illustrations:
+        # transform data in dictionnary and concatenate
+        keywords_existing_illustrations.update({d['keyword']: d for d in keywords_infos})
+        _saveKeywords_Illustration(keywords_existing_illustrations)
+    return keywords_infos
+
 
 def _imagenameformat(url,imagename=None,background=False):
     '''
@@ -50,57 +146,9 @@ def saveImage(url : str,imagename=None,background=False):
     return imagename
     
 
-
-def _updateKeywords(period):
-    #TODO: interact with tweeter (by example) to find keywords fitting to a period
-    return ["griezmann","vincent collet","mario"]
-    
-def _initPeriods():
-    #TODO: load all available periods on init 
-    return ["Decembre 2020", "Janvier 2021","Fevrier 2021"]
-
-def _initHTMLCSSJSFiles():
-    indexlines = []
-    # read the template
-    with open('../uitemplate.html', 'r') as template:
-        for line in template:
-            if '<!--datestopropose-->' in line:
-                # create dropdown list containing all the available periods
-                for period in _initPeriods():
-                    name = period.replace(' ','-').lower()
-                    option = f'<option value="{name}">{period}</value>'
-                    indexlines.append(option)
-            else :  indexlines.append(line.strip())
-
-    # write the ui file
-    with open(UI_FOLDER+"/index.html", 'w') as fileindex:
-        fileindex.write('\n'.join(indexlines))
-
-
-def startui():
-   if not os.path.exists(UI_FOLDER): os.mkdir(UI_FOLDER)
-   _initHTMLCSSJSFiles()
-   _createImagesFolder()
-   eel.init(UI_FOLDER)  
-
-   @eel.expose
-   def loadPeriodPy(periodid):
-    '''
-    Applies changes on the UI, according to a new period selection by the user
-    The keywords of the periods are retreived
-    The  illustration images are downloaded and their background are removed
-    '''
-    wikip = WikiParser
-    gisp = GImageSerpApiParser
-    keywords = _updateKeywords(periodid)
-    eel.loadPeriodJs(_illustrate_keywords(keywords,[wikip, gisp]))
-
-   eel.start('index.html',close_callback=_deleteUIfolder,size=(700,700))
-
-
 def _findImageLinks(parser : ImageParser,keywordsurls:dict[str,str]):
     '''
-    tries to illustrate (find an url) the maximum of keywords with the given parser
+    tries to illustrate (find an url) the maximum of not illustrated keywords with the given parser
     '''
     for keyword in keywordsurls:
         url = keywordsurls[keyword]
@@ -108,28 +156,14 @@ def _findImageLinks(parser : ImageParser,keywordsurls:dict[str,str]):
             keywordsurls[keyword] = parser.getImageLink(keyword)
     return keywordsurls
 
-def _illustrate_keywords(keywords : list[str],parsers : list[ImageParser]):
-    '''
-    tries to illustrate the maximum of keywords with the parsers.
-    returns a list of     objects with the shape :  
-    { keyword,  url, illustration} indicating all the illustrated keywords.
-    url and illustration can be empty strings if the parsers failed to find an illustration.
-    '''
-    keywords_url = {key: '' for key in keywords}
-    for parser in parsers:
-        keywords_url = _findImageLinks(parser,keywords_url)
+def _getSavedKeywordsIllustrations():
+    try:
+        with open(UI_FOLDER+"/keywords_illustration.json", "r") as json_file:
+            return json.load(json_file)
+    except FileNotFoundError:
+        return {}
     
-    #Resets the images folder content and loads all the new images in it
-    # for f in glob.glob(UI_FOLDER+"/images/*"):os.remove(f)
-    keywords_infos = []
-    for keyword in list(keywords_url.keys()):
-         url = keywords_url[keyword]
-         illustration =  _imagenameformat(url,keyword)
-         if not os.path.exists(UI_FOLDER+'/images/'+ illustration): 
-             saveImage(url,keyword) 
-         keywords_infos.append({
-             "keyword":keyword,
-             "url":url,
-             "illustration":illustration
-         })
-    return keywords_infos
+def _saveKeywords_Illustration(keywords_infos):
+    with open(UI_FOLDER+"/keywords_illustration.json", "w") as json_file:
+         # Write the dictionary to the file as JSON data
+         json.dump(keywords_infos, json_file)
